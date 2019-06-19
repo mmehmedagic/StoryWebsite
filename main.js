6dbc8172b832d88e712781a59473adf6b8f04d2f
Mirza Mehmedagic <mmehmedagic@cps.edu>
2019-06-19 11:54:52 -0500

    Add final notes and remove JS height code

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
+var pictures = ["pic1", "pic2"];
+
 window.onresize = function() {
-    var img = document.getElementById("pic1");
-    var ratio = img.width / img.height;
-    var rect = img.getBoundingClientRect();
-    var isOffscreen = (
-      rect.x + rect.width < 0 
-   || rect.y + rect.height < 0 
-   || rect.x + rect.width > window.innerWidth 
-   || rect.y + rect.height > window.innerHeight
-    );
-    document.getElementById("test").innerHTML = isOffscreen;
-    if (isOffscreen) {
-      var height = (window.innerHeight - rect.y - 5) + "px";
-      var width = (height * ratio) + "px";
-      img.style.height = height;
-      img.style.width = width;
-    } else {
-      img.style.width = "100%";
+    for (i = 0; i < pictures.length; i++) {
+        var img = document.getElementById(pictures[i]);
+        img.style.width = "100%";
+        
+        // Work in progress
+
+        // var ratio = img.width / img.height;
+        // var rect = img.getBoundingClientRect();
+        // var isOffscreen = (
+        //     rect.x + rect.width < 0 
+        //  || rect.y + rect.height < 0 
+        //  || rect.x + rect.width > window.innerWidth 
+        //  || rect.y + rect.height > window.innerHeight
+        // );
+        // if (isOffscreen) {
+        //     img.style.height = (window.innerHeight - rect.y - 5) + "px";
+        //     img.style.width = (height * ratio) + "px";
+        // } else {
+        //  
+        // }
+
     }
-  };
+};
